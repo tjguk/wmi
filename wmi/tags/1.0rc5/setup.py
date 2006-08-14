a263c6b4d2b9fea901b18e0fce99ac92cbc62d0f
tgolden <tgolden@8d7aa616-f61a-0410-9694-9bde58757701>
2006-08-14 09:16:20 +0000

    Tag WMI version 1.0rc5

---
+++ wmi/tags/1.0rc5/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Environment :: Win32 (MS Windows)',
+  'Intended Audience :: Developers',
+  'Intended Audience :: System Administrators',
+  'License :: PSF',
+  'Natural Language :: English',
+  'Operating System :: Microsoft :: Windows :: Windows 95/98/2000',
+  'Topic :: System :: Systems Administration'
+]
+
+setup (
+  name = "WMI",
+  version = "1.0rc5",
+  description = "Windows Management Instrumentation",
+  author = "Tim Golden",
+  author_email = "mail@timgolden.me.uk",
+  url = "http://timgolden.me.uk/python/wmi.html",
+  license = "http://www.opensource.org/licenses/mit-license.php",
+  py_modules = ["wmi"]
+)
+
