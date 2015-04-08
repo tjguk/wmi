84d29fcc5baee75ee0548a907d7cc945cc058480
Tim Golden <mail@timgolden.me.uk>
2015-04-08 17:49:49 +0100

    Bring Copyright notice up to date

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ master_doc = 'contents'
 
 # General information about the project.
 project = u'WMI'
-copyright = u'2009, Tim Golden'
+copyright = u'2015, Tim Golden'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
